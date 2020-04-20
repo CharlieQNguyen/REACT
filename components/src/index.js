b317c82858e8606545e5974b077dbd526acf4482
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-04-19 23:43:03 -0700

    component continual

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
       <div className="ui container comments">
-        <CommentDetail author = "Sam"/>
-        <CommentDetail author = "Alex"/>
-        <CommentDetail author = "Jane"/>
+        <ApprovalCard>
+          <CommentDetail
+            author="Sam"
+            timeAgo="Today at 4:45PM"
+            content="this is cool"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Alex"
+            timeAgo="Today at 2:00PM"
+            content="nice work"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Jane"
+            timeAgo="Today at 5:00PM"
+            content="like my comment"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
       </div>
     );
 };
