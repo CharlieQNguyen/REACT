d28de3fff5b678251d8847df8025b6700106e6fd
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-04-29 17:25:20 -0700

    seasons update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+        // to this.state when it is instantiated in the construtor
+        this.state = { lat: null, errorMessage: ''};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+          position => {
+              // we called setstate!!!
+            this.setState({ lat: position.coords.latitude})
+          },
+          err => {
+            this.setState({errorMessage: err.message})
+          }
         );
-        return <div>Latitude:</div>
+    }
+
+    //React says we have to define render!!
+    render() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
     }
 }
 
