d25322db1cfc6f1be62c935fe8f9948cfb017d68
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-12 17:38:22 -0700

    thumbnail updated

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
+// map over videos array for every Video inside VideoList we will render 1 single VideoItem component
 const VideoList = ({videos}) => {
-    // props.videos
-    return <div>{videos.length}</div>;
+    // take video array from APP compenent and map over it
+    const renderList = videos.map((video) => { //inside video object we are refering to many objects we refer to as videos so it will be call one time for every video object inside the video array
+        return <VideoItem video={video} />;
+    })
+
+    return <div>{renderList}</div>;
 };
 
 export default VideoList;
