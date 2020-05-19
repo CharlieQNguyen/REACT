89980868d8c4f5844f49124eb8e2c089647c7427
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-19 15:11:16 -0700

    reducers combined

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'; // importing redux combineReducer function in redux library
+
 const songsReducer = () => { //going to be a static list array/songs
     return [
         { title: 'No Scrubs', duration: '4:05' },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
