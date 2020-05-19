104931a5d417bd300219e32d5c06b02157740ac7
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-19 16:21:08 -0700

    connect to be add

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {
     return(
--- songs/src/index.js
+++ songs/src/index.js
@@ -1,7 +1,16 @@
+// all imports from outside dependency 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // Provider is a component from react-redux
+import { createStore } from 'redux';    // createStore function fromm redux library
+// all imports created internally 
 import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
-    <App />, document.querySelector('#root')
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
+    
+    document.querySelector('#root')
 );
