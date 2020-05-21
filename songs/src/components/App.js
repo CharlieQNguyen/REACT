09472c2ab91b544136266085c3276f2cc6ce039d
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-20 18:40:19 -0700

    added css grid

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -3,8 +3,14 @@ import SongList from './SongList';
 
 const App = () => {
     return(
-        <div>App</div>
-    )
-}
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
