bc2f092a2c32334b874d3b43e5711837dcb9c689
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-05-28 11:37:48 -0700

    fetchUser action created

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,15 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// action creater to fetch all posts
 export const fetchPosts = () => async dispatch => { // dispatch allow us to change any data, getState allows us to get any data from store
-        const response = await jsonPlaceholder.get('/posts')
+        const response = await jsonPlaceholder.get('/posts');
 
-        dispatch({type: 'FETCH_POSTS', payload: response.data }) // we only about the data property in this api not the whole object
+        dispatch({ type: 'FETCH_POSTS', payload: response.data }); // we only about the data property in this api not the whole object
+};
+
+// action creater to fetch one user at a time
+export const fetchUser = (id) => async dispatch => {
+        const response = await jsonPlaceholder.get(`/users/${id}`) //(`/users/${id}`) is 2015 js syntax is same as ('/users/' + id)
+
+        dispatch({ type: 'FETCH_USER', payload: response.data });
 };
