2c5bef50c6f23313a8142bc7b8179c4a30eb0093
Charlie Nguyen <charlie.nguyen43@gmail.com>
2020-06-01 19:32:06 -0700

    fetch posts and users created

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+
+        const userIds = _.uniq(_.map(getState().posts, 'userId')) // getting all the posts in an array with unique userId
+        userIds.forEach(id => dispatch(fetchUser(id))) //for each id we are going to call the fetchuser and dispatch 
+}
+
 // action creater to fetch all posts
 export const fetchPosts = () => async dispatch => { // dispatch allow us to change any data, getState allows us to get any data from store
         const response = await jsonPlaceholder.get('/posts');
